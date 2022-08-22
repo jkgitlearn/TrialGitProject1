066ec61454beb8645d199ae549661f498cbfc411
jkgitlearn <shilpi_rams@yahoo.com>
2022-08-22 00:29:12 -0400

    first commit for trial git project1

---
+++ src/gitProject/Test1.java
@@ -0,0 +1,19 @@
+package gitProject;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+	
+		String hungry = "yes";
+		
+		if(hungry == "yes")
+		{
+			System.out.println("eat something");
+		}
+		else
+		{
+			System.out.println("Go to Bed");
+		}
+	}
+
+}
